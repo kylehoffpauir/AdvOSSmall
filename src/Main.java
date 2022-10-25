9da4f9e37324800f98f56a23c13b3574e190aed8
kolidesa <77902590+kolidesa@users.noreply.github.com>
2022-10-25 12:34:57 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         String algo = args[1];
 
         //Get quantum (if applicable)
-        int quantum;
+        int quantum = NULL;
         if (args.length == 5) {
             quantum = Integer.parseInt(args[3]);
         }
@@ -17,7 +17,21 @@ public class Main {
 
 
 
+        //Go through file and use algos
 
 
+        //Create output file
+        PrintWriter output = new PrintWriter("results.txt");
+        output.println("Input File Name : " + fileName);
+        output.print("CPU Scheduling Alg : " + algo);
+        if (quantum == NULL)
+            output.println("(" + quantum + ")");
+        else output.println();
+        output.println("CPU utilization : " + utilization);
+        output.println("Throughput : " + throughput);
+        output.println("Turnaround time : " + turnaroundTime);
+        output.println("Waiting time : " + waitingTime);
+        output.println("Response time : " + responseTime);
+        output.close();
     }
 }
