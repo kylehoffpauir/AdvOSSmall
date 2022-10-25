36910e8e7449306855030ab829b562ce03554ec9
kolidesa <77902590+kolidesa@users.noreply.github.com>
2022-10-25 12:28:18 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-	    //Get algorithm
+	   //Get algorithm
         String algo = args[1];
 
         //Get quantum (if applicable)
