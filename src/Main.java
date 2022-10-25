7fcef0dec0700458ec4e52acc3e49e6f805fc212
kolidesa <77902590+kolidesa@users.noreply.github.com>
2022-10-25 12:27:34 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.*;
+import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    //Get algorithm
+        String algo = args[1];
+
+        //Get quantum (if applicable)
+        int quantum;
+        if (args.length == 5) {
+            quantum = Integer.parseInt(args[3]);
+        }
+
+        //Get input file name
+        String fileName = args[args.length - 1];
+
+
+
+
+
     }
 }
