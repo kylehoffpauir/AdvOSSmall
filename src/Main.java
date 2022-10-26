f9c79453362f389a0e8dc8c78f50ee81dc75934e
Kyle <kyle@hoffpauir.com>
2022-10-26 13:44:34 -0400

    threads created

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
 	public static String algo = "";
 	public static int quantum = -1;
 	private static final boolean DEBUG = true;
-	protected static PriorityQueue<Process> q = new PriorityQueue<Process>();
+	protected static LinkedList<Process> readyQueue = new LinkedList<Process>();
+	protected static LinkedList<Process> ioQueue = new LinkedList<Process>();
 
 
 	public static void main(String[] args) {
@@ -57,59 +58,6 @@ public class Main {
 
 	//a mess
 	public static void readProc(File inFile) {
-		Scanner f = null;
-		try {
-			f = new Scanner(inFile);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		//linecount used to give proc priorities based on FCFS
-        //do we even need this? depending on the implementation yes
-        //if the process enters the Q with prioirtity based on it's input, we can deQ, do something, and re-enQ
-        //this will make it so that the process now have the minimum priority and can continue on.
-		int lineCount = 1;
-		while (f.hasNextLine()) {
-		    //split contents of input into array
-			String[] items = f.nextLine().split(" ");
-			//if it's a sleep then we sleep the readThread
-			if (items[0].toLowerCase().equals("sleep")) {
-				try {
-				    //TODO how? and where? do we sleep the filethread.
-					fileThread.sleep(Integer.parseInt(items[1]));
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			} else if (items[0].toLowerCase().equals("proc")) {
-				Process p = new Process();
-				//only care about the priority for PRI or SPN, FCFS is prioritized based on sequence order
-				if (algo.equals("PRI") || algo.equals("SPN"))
-					p.setPriority(Integer.parseInt(items[1]));
-				if (algo.equals("FCFS")) {
-					p.setPriority(lineCount);
-				} else
-					p.setPriority(1);
-                //extract the data from the proc lines
-				for (int i = 2; i < items.length; i++) {
-					//cpu is even items
-					if (i % 2 == 0) {
-						p.addCpu(Integer.parseInt(items[i]));
-					}
-					//io is odd items
-					else {
-						p.addIo(Integer.parseInt(items[i]));
-					}
-				}
-				if (DEBUG) System.out.println(p);
-				q.add(p);
-			} else if (items[0].toLowerCase().equals("stop")) {
-			  //if we run into a stop then we end the reading
-				return;
-			} else {
-				System.err.println("Error - input file has unexpected command");
-				System.exit(3);
-			}
-			if(lineCount < 10) lineCount++;
-		}
 	}
 
 	public static void sendOut(File inFile) {
@@ -139,24 +87,24 @@ public class Main {
 
 	//100
 	public static void FCFS(File inFile) {
-	  //TODO establish how to actually run the threads and simulate the processes
-      //does this need to be it's own class as well? does readProc need to be a member of the fileThread class
-      //im thinking maybe
-		try {
-			Thread fileThread = new Thread(new Runnable() {
-				@Override
-				public void run() {
-					readProc(inFile);
-				}
-			});
-			fileThread.start();
-			CpuThread cpuThread = new CpuThread();
-			cpuThread.start();
-			//sleep to give the filethread a chance to actually put something in the queue
-			cpuThread.sleep(50);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		//TODO establish how to actually run the threads and simulate the processes
+		//does this need to be it's own class as well? does readProc need to be a member of the fileThread class
+		//im thinking maybe
+		FileThread ft = new FileThread(inFile);
+		CpuThread cput = new CpuThread();
+		IoThread iot = new IoThread();
+		ft.start();
+		cput.start();
+		iot.start();
+		//bad, maybe find a better way eventually?
+		while(true) {
+			if (ft.isInterrupted() && readyQueue.isEmpty() && ioQueue.isEmpty()) {
+				cput.interrupt();
+				iot.interrupt();
+				break;
+			}
 		}
+		//TODO get output out of this?
 	}
 
 	//25
@@ -173,4 +121,153 @@ public class Main {
 	public static void RR(File file) {
 
 	}
-}
+
+	public static class FileThread extends Thread {
+		public static File inFile;
+		public static boolean exit = false;
+
+		public FileThread(File inFile) {
+			this.inFile = inFile;
+		}
+
+		@Override
+		public void run() {
+			while(!exit) {
+				Scanner f = null;
+				try {
+					f = new Scanner(inFile);
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+				//linecount used to give proc priorities based on FCFS
+				//do we even need this? depending on the implementation yes
+				//if the process enters the Q with prioirtity based on it's input, we can deQ, do something, and re-enQ
+				//this will make it so that the process now have the minimum priority and can continue on.
+				int lineCount = 1;
+				while (f.hasNextLine()) {
+					//split contents of input into array
+					String[] items = f.nextLine().split(" ");
+					//if it's a sleep then we sleep the fileThread
+					if (items[0].toLowerCase().equals("sleep")) {
+						try {
+							//TODO how? and where? do we sleep the filethread.
+							this.sleep(Integer.parseInt(items[1]));
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					} else if (items[0].toLowerCase().equals("proc")) {
+						Process p = new Process();
+						//only care about the priority for PRI or SPN, FCFS is prioritized based on sequence order
+						if (algo.equals("PRI") || algo.equals("SPN"))
+							p.setPriority(Integer.parseInt(items[1]));
+						else if (algo.equals("FCFS")) {
+							p.setPriority(lineCount);
+						} else
+							p.setPriority(1);
+						//extract the data from the proc lines
+						for (int i = 2; i < items.length; i++) {
+							//cpu is even items
+							if (i % 2 == 0) {
+								p.addCpu(Integer.parseInt(items[i]));
+							}
+							//io is odd items
+							else {
+								p.addIo(Integer.parseInt(items[i]));
+							}
+						}
+						if (DEBUG) System.out.println(p);
+						readyQueue.add(p);
+					} else if (items[0].toLowerCase().equals("stop")) {
+						//if we run into a stop then we end the reading
+						this.exit = true;
+					} else {
+						System.err.println("Error - input file has unexpected command");
+						System.exit(3);
+					}
+					if (lineCount < 10) lineCount++;
+				}
+			}//end the !exit loop
+			return;
+		}
+	}
+
+
+	public static class CpuThread extends Thread {
+		/*
+		 * The CPU scheduler thread will check the ready queue; if there is a process, it will pick one according to the
+		 * scheduling algorithm from the ready queue and hold the CPU resource for the given CPU service time
+		 * (or for quantum time if the scheduling algorithm is RR). That means the CPU thread will simply sleep for the
+		 * given CPU service time. Then it will release the CPU resource and put this process into the IO queue
+		 * (or the ready queue if RR is used) or just terminate if there is no more CPU service.
+		 * Then the CPU scheduler thread will check the ready queue again and repeat.
+		 */
+		public static boolean exit = false;
+
+
+		@Override
+		public void run() {
+			while (!exit) {
+				if(!readyQueue.isEmpty()) {
+					Process currentProc = algoRemove();
+					try {
+						//remove the first element from the process' cpu queue
+						Thread.sleep(currentProc.removeCpu());
+						ioQueue.add(currentProc);
+					} catch (InterruptedException e) {
+						//e.printStackTrace();
+						exit = true;
+					}
+				}
+			}//end !exit loop
+		}
+
+		public static Process algoRemove() {
+			switch (algo) {
+				case "FCFS":
+					return readyQueue.remove(0);
+				case "RR":
+					return readyQueue.remove(0);
+				case "SPN":
+					return readyQueue.remove(0);
+				case "PRI":
+					return readyQueue.remove(0);
+				default:
+					System.err.println("Error - cpu error when attempting to dequeue a process");
+					System.exit(4);
+					return new Process();
+			}
+		}
+	}//end CpuThread
+
+
+	public static class IoThread extends Thread {
+		/*
+		 * The I/O system thread will check the IO queue; if there is a process,
+		 * it will hold the IO device for the given IO service time.
+		 * That is, the IO thread will sleep for the given IO service time.
+		 * It then puts this process back into the ready queue.
+		 * Finally it will check the IO queue and repeat.
+		 */
+		public static boolean exit = false;
+
+		@Override
+		public void run() {
+			while (!exit) {
+				if(!ioQueue.isEmpty()) {
+					Process currentProc = ioQueue.remove(0);
+					try {
+						//remove the first element from the process' cpu queue
+						Thread.sleep(currentProc.removeIo());
+						readyQueue.add(currentProc);
+					} catch (InterruptedException e) {
+						//e.printStackTrace();
+						exit = true;
+					}
+				}
+			}//end !exit loop
+		}
+	}//end IoThread
+
+
+
+}//end class
